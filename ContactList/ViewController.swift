c867013e4c8829956bee2e5cd48d1ef88facf99a
Морозов Евгений <evgenii.morozov@sparm.com>
2025-03-05 20:39:28 +0300

    Initial Commit

---
+++ ContactList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactList
+//
+//  Created by Evgeniy Morozov on 05.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
